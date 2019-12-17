a9e3327395afe54aef60d06906651a3a6c4dfdb0
Abi Krish <abikrish@Abis-MacBook-Pro.local>
2019-12-16 18:24:21 -0700

    Made object-oriented, added edit injury option

--- injurystatus.py
+++ injurystatus.py
@@ -3,81 +3,97 @@ import lxml.html as lh
 import pandas as pd
 import re
 
-url='https://www.cbssports.com/nba/injuries/'
-# Create a handle, page, to handle the contents of the website
-page = requests.get(url)
-# Store the contents of the website under doc
-doc = lh.fromstring(page.content)
-# Parse data that are stored between <tr>..</tr> of HTML
-tr_elements = doc.xpath('//tr')
-tr_elements = doc.xpath('//tr')
+class InjuryTable:
+    def __init__(self, url='https://www.cbssports.com/nba/injuries/'):
+        # Create a handle, page, to handle the contents of the website
+        page = requests.get(url)
+        # Store the contents of the website under doc
+        doc = lh.fromstring(page.content)
+        # Parse data that are stored between <tr>..</tr> of HTML
+        tr_elements = doc.xpath('//tr')
+        tr_elements = doc.xpath('//tr')
 
-# Create empty list
-col = []
-i = 0
+        # Create empty list
+        col = []
+        i = 0
 
-# For each row, store each first element (header) and an empty list
-for t in tr_elements[0]:
-    i += 1
-    name = t.text_content()
-    # print('%d:"%s"'%(i,name.strip()))
-    col.append((name.strip(),[]))
-# print(col)
+        # For each row, store each first element (header) and an empty list
+        for t in tr_elements[0]:
+            i += 1
+            name = t.text_content()
+            # print('%d:"%s"'%(i,name.strip()))
+            col.append((name.strip(),[]))
+        # print(col)
 
-#Since out first row is the header, data is stored on the second row onwards
-for j in range(1,len(tr_elements)):
-    #T is our j'th row
-    T = tr_elements[j]
+        #Since out first row is the header, data is stored on the second row onwards
+        for j in range(1,len(tr_elements)):
+            #T is our j'th row
+            T = tr_elements[j]
 
-    #If row is not of size 5, the //tr data is not from our table
-    if len(T) != 5:
-        break
-
-    #i is the index of our column
-    i = 0
-
-    #Iterate through each element of the row
-    for t in T.iterchildren():
-        data = t.text_content()
-        #Modify first cell
-        if i == 0:
-            #remove redundant headings
-            if data.strip() == 'Player':
+            #If row is not of size 5, the //tr data is not from our table
+            if len(T) != 5:
                 break
-            #remove abbreviated name
-            else:
-                data = data.strip()
-                data = re.sub(r'.*\n', '\n', data)
-                data = data.strip()
-                if data == 'Moe Wagner':
-                    data = 'Moritz Wagner'
-        #Check if row is empty
-        if i > 0:
-        #Convert any numerical value to integers
-            try:
-                data = float(data.strip())
-            except:
-                pass
-        #Append the data to the empty list of the i'th column
-        col[i][1].append(data.strip())
-        #Increment i for the next column
-        i += 1
 
-def addsuspension(col, Player, Pos, UpdateDate, EndDate):
-    temp = col
-    temp[0][1].append(Player)
-    temp[1][1].append(Pos)
-    temp[2][1].append(UpdateDate)
-    temp[3][1].append('Suspension')
-    temp[4][1].append('Expected to be out until at least '+ EndDate)
-    return temp
+            #i is the index of our column
+            i = 0
 
-col = addsuspension(col, 'Deandre Ayton', 'C', 'Oct 28', 'Dec 17')
-col = addsuspension(col, 'John Collins', 'PF', 'Nov 4', 'Dec 23')
+            #Iterate through each element of the row
+            for t in T.iterchildren():
+                data = t.text_content()
+                #Modify first cell
+                if i == 0:
+                    #remove redundant headings
+                    if data.strip() == 'Player':
+                        break
+                    #remove abbreviated name
+                    else:
+                        data = data.strip()
+                        data = re.sub(r'.*\n', '\n', data)
+                        data = data.strip()
+                        if data == 'Moe Wagner':
+                            data = 'Moritz Wagner'
+                #Check if row is empty
+                if i > 0:
+                #Convert any numerical value to integers
+                    try:
+                        data = float(data.strip())
+                    except:
+                        pass
+                #Append the data to the empty list of the i'th column
+                col[i][1].append(data.strip())
+                #Increment i for the next column
+                i += 1
+        self.col = col
 
 
-[len(C) for (title,C) in col]
+    def addinjury(self, Player, Pos, UpdateDate, Injury, EndDate):
+        self.col[0][1].append(Player)
+        self.col[1][1].append(Pos)
+        self.col[2][1].append(UpdateDate)
+        self.col[3][1].append(Injury)
+        self.col[4][1].append('Expected to be out until at least '+ EndDate)
 
-Dict = {title:column for (title,column) in col}
-df = pd.DataFrame(Dict)
-df.to_excel("injurydata.xlsx")
+    def addsuspension(self, Player, Pos, UpdateDate, EndDate):
+        self.col[0][1].append(Player)
+        self.col[1][1].append(Pos)
+        self.col[2][1].append(UpdateDate)
+        self.col[3][1].append('Suspension')
+        self.col[4][1].append('Expected to be out until at least '+ EndDate)
+
+    def databaseprint(self):
+        Dict = {title:column for (title,column) in self.col}
+        df = pd.DataFrame(Dict)
+        df.to_excel("injurydata.xlsx")
+
+    def editinjury(self, Player, Pos, UpdateDate, Injury, EndDate):
+        if Player in self.col[0][1]:
+            a = self.col[0][1].index(Player)
+            (self.col[1][1])[a] = Pos
+            (self.col[2][1])[a] = UpdateDate
+            (self.col[3][1])[a] = Injury
+            (self.col[4][1])[a] = 'Expected to be out until at least '+ EndDate
+table = InjuryTable()
+
+table.addsuspension('Deandre Ayton', 'C', 'Oct 28', 'Dec 17')
+table.addsuspension('John Collins', 'PF', 'Nov 4', 'Dec 23')
+table.databaseprint()
